e02c97219257610fe95b6307aac0e64818d582d0
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-09-28 08:39:39 -0700

    Add the connection rate and its update and get functions

--- NetworkGraphExpression/NetworkGraphExpression.m
+++ NetworkGraphExpression/NetworkGraphExpression.m
@@ -4,6 +4,7 @@ classdef NetworkGraphExpression < handle
         node_positions
         adjacent_matrix
         range_threshold
+        connection_rate
     end
     methods
         function obj = NetworkGraphExpression(args)
@@ -11,7 +12,9 @@ classdef NetworkGraphExpression < handle
             obj.node_positions = args.node_positions;
             obj.adjacent_matrix = zeros(args.num_nodes, args.num_nodes);
             obj.range_threshold = args.range_threshold;
+            obj.connection_rate = zeros(1, args.num_nodes);
         end
+
         function updateAdjacentMatrixByRange(this)
             NUM_NODES = this.num_nodes;
             for iNode = 1:NUM_NODES-1
@@ -26,6 +29,15 @@ classdef NetworkGraphExpression < handle
                     end
                 end
             end
+            this.updateConnectionRate();
+        end
+
+        function updateConnectionRate(this)
+            NUM_NODES = this.num_nodes;
+            for iNode = 1:NUM_NODES
+                sum_adjacent = sum(this.adjacent_matrix(iNode,:));
+                this.connection_rate(1, iNode) = sum_adjacent/(NUM_NODES-1);
+            end
         end
 
         % Set functions
@@ -37,6 +49,9 @@ classdef NetworkGraphExpression < handle
         end
 
         % Get functions
+        function output = getConnectionRate(this)
+            output = this.connection_rate;
+        end
         function output = getAdjacentMatrix(this)
             output = this.adjacent_matrix;
         end
